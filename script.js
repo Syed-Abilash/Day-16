edcc1dfbf668fa7e8b87d6db6461979d0ce4afcc
Syed-Abilash <iquwansyed09@gmail.com>
2023-11-27 11:47:07 +0530

    day-16 task

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("task").innerText = "10";
+    setTimeout(() => {
+      document.getElementById("task").innerText = "9";
+      setTimeout(() => {
+        document.getElementById("task").innerText = "8";
+        setTimeout(() => {
+          document.getElementById("task").innerText = "7";
+          setTimeout(() => {
+            document.getElementById("task").innerText = "6";
+            setTimeout(() => {
+              document.getElementById("task").innerText = "5";
+              setTimeout(() => {
+                document.getElementById("task").innerText = "4";
+                setTimeout(() => {
+                  document.getElementById("task").innerText = "3";
+                  setTimeout(() => {
+                    document.getElementById("task").innerText = "2";
+                    setTimeout(() => {
+                      document.getElementById("task").innerText = "1";
+                      setTimeout(() => {
+                        document.getElementById("task").innerText =
+                          "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
